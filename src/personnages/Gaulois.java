8744d1593dc1e9c20a2a95080fff242ffda5075f
Lady1704 <celine.cholin@gmail.com>
2023-10-07 22:41:31 +0200

    TP3

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,10 +4,12 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	private int nb_trophees;
+	private Equipement[] trophees;
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
-		 
+		this.trophees[nb_trophees] = trophees[i];
 	}
 	public String getNom() {
 		return nom;
@@ -15,20 +17,38 @@ public class Gaulois {
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+//	private String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
+	
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
+		String texte = " Le gaulois " + nom + " : ";
+		return texte;
 	}
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+//		+ romain.getNom());
+//		romain.recevoirCoup(force / 3);
+//	}
+	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
-		+ romain.getNom());
-		romain.recevoirCoup(force / 3);
+		System.out.println(nom + " envoie un grand coup dans la "
+				+ "mâchoire de " + romain.getNom());
+		Equipement trophees[i] = romain.recevoirCoup((force / 3) *
+				effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++,
+				nb_trophees++) {
+		}
+		//return;
 	}
+	
 	public void boirePotion(int forcePotion) {
         effetPotion = forcePotion;
         if (forcePotion == 3) {
             parler("Merci Druide, je sens que ma force est 3 fois décuplée.");
         }
     }
+	
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force
