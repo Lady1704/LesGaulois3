92262538c7ccabd581dc2eb7be39f0c8b711b7c8
Lady1704 <celine.cholin@gmail.com>
2023-10-09 21:16:33 +0200

    TP3

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -2,38 +2,65 @@ package personnages;
 
 public class Gaulois {
 	private String nom;
-	private int force;
 	private int effetPotion = 1;
+	private int force;
+	private int nbtrophees;
+	private Equipement trophees[] = new Equipement[100];
+	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 		 
 	}
+	
 	public String getNom() {
 		return nom;
 	}
+	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+	
+//	private String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
+	
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
-	}
+		String texte = "Le gaulois " + nom + " : ";
+		return texte;
+		}
+	
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+//		+ romain.getNom());
+//		romain.recevoirCoup(force / 3);
+//	}
+	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
-		+ romain.getNom());
-		romain.recevoirCoup(force / 3);
+		System.out.println(nom + " envoie un grand coup dans la"
+				+ "mâchoire de " + romain.getNom());
+		Equipement trophees[] = romain.recevoirCoup((force / 3) *
+				effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++,
+				nb_trophees++) {
+			this.trophees[nb_trophees] = trophees[i];
+		}
+		return;
 	}
+	
 	public void boirePotion(int forcePotion) {
         effetPotion = forcePotion;
         if (forcePotion == 3) {
             parler("Merci Druide, je sens que ma force est 3 fois décuplée.");
         }
     }
+	
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force
 		+ ", effetPotion=" + effetPotion + "]";
 	}
+	
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix",8);
 		Romain romain = new Romain("Minus", 4);
